7915fc5b9ad8f0ed5457378a5a7e835e3eaeed64
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-10-06 15:37:21 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import PokemonList from './components/pokemon-list';
 import store from './redux/store';
 
 export default class App extends Component {
-  render() {
-    return (
-      <Provider store = {store}>
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/pokemon-list"/>
-            </Route>
-            <Route exact path="/pokemon-list" component={PokemonList}/>
-            <Route exact path="/pokemon-list/:id" component={PokemonItem}/>
-            <Route path="/pokemon-list/:id/evolution-chain" component={EvolutionChain}/>
-          </Switch>
-        </Router>
-      </Provider>
-    )
-  }
+    render() {
+        return (
+            <Provider store = {store}>
+                <Router>
+                      <Switch>
+                          <Route exact path="/">
+                            <Redirect to="/pokemon-list"/>
+                          </Route>
+                          <Route exact path="/pokemon-list" component={PokemonList}/>
+                          <Route exact path="/pokemon-list/:id" component={PokemonItem}/>
+                          <Route path="/pokemon-list/:id/evolution-chain" component={EvolutionChain}/>
+                    </Switch>
+                </Router>
+            </Provider>
+        )
+    }
 }
